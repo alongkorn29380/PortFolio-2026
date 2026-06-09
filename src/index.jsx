4a55dc2d12a3975810c8602cb450f6422b4195a0
Alongkorn Anuwatprakit <alongkorn29380@gmail.com>
2026-06-09 14:42:20 +0700

    feat: transparent canvas + updated camera for card layout

--- src/index.jsx
+++ src/index.jsx
@@ -4,18 +4,22 @@ import { Canvas } from '@react-three/fiber'
 
 import Experience from './Experience.jsx'
 import Navebar from './Components/Navebar/Navebar'
+import Profile from './Components/Profile/Profile.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <>
         <Navebar />
+        <Profile />
         <Canvas
+            style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 0 }}
+            gl={{ alpha: true }}
             camera={{
                 fov: 45,
                 near: 0.1,
                 far: 2000,
-                position: [ -3, 1.5, 4 ]
+                position: [ 0, 1, 7 ]
             }}
         >
             <Experience />
